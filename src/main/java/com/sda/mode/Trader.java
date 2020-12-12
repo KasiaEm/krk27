8676c42f205a99a4585563d617bae1c1cd1cc7c9
K Musioł <katarzyna.musiol@ing.com>
2020-12-12 13:38:24 +0100

    ReceiveDamage - Trader

---
+++ src/main/java/com/sda/mode/Trader.java
@@ -0,0 +1,13 @@
+package com.sda.mode;
+
+import com.sda.model.other.PricedItem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Trader {
+    private String name;
+    private Map<String, PricedItem> stock = new HashMap<>();
+
+    
+}
---
+++ src/main/java/com/sda/model/other/PricedItem.java
@@ -0,0 +1,19 @@
+package com.sda.model.other;
+
+public class PricedItem {
+    private int price;
+    private InventoryObject item;
+
+    public PricedItem(int price, InventoryObject item) {
+        this.price = price;
+        this.item = item;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public InventoryObject getItem() {
+        return item;
+    }
+}
