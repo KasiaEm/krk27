c2b3fe10e9a7dd00f217ceaf36de568089401c8b
K Musioł <katarzyna.musiol@ing.com>
2020-12-13 13:19:10 +0100

    Game

--- src/main/java/com/sda/Game.java
+++ src/main/java/com/sda/Game.java
@@ -2,13 +2,28 @@ package com.sda;
 
 public class Game {
     private static char map[][];
+    private static Position heroPos;
+    private static Position finishPos;
 
     public static void main(String[] args) {
         init();
     }
 
 
-    private static void init(){
+    private static void init() {
         map = FileService.loadMap();
+        heroPos = findChar('H');
+        finishPos = findChar('F');
+    }
+
+    private static Position findChar(char toFind){
+        for (int i=0; i < map.length; i++){
+            for(int j=0; j <map[i].length; j++){
+                if(map[i][j]==toFind){
+                    return new Position(j,i);
+                }
+            }
+        }
+        return null;
     }
 }
